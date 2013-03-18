279b72d72adbca0eeb669f608c8dee065dc82ee8
cfeliciano <c.feliciano2009@gmail.com>
2013-03-17 20:03:26 -0400

    Added style to error messages. Users can now succesfully create a page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -19,20 +19,23 @@ describe "User pages" do
     it { should have_selector('title', text: user.name) }
   end
 
-  describe "signup" do 
+  describe "signup" do
+    
+    before { visit signup_path }
+    
     let(:submit) { "Create my account" }
-
+    
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
     end
-
+    
     describe "with valid information" do
-      before do 
-        fill_in "Name", with: "Example User"
-        fill_in "Email", with: "user@example.com"
-        fill_in "Password", with: "foobar"
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
       
@@ -40,5 +43,6 @@ describe "User pages" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
     end
-  end
+  end 
+
 end
