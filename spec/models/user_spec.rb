830e355262995cf9b3d22520eb4ed90c610976af
cfeliciano <c.feliciano2009@gmail.com>
2013-03-19 22:17:13 -0400

    Added admin to the database

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,7 @@
 #  updated_at      :datetime         not null
 #  password_digest :string(255)
 #  remember_token  :string(255)
+#  admin           :boolean          default(FALSE)
 #
 
 require 'spec_helper'
@@ -124,4 +125,20 @@ describe User do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
   end
+
+  it { should respond_to(:admin) }
+  it { should respond_to(:authenticate) }
+
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+
 end
