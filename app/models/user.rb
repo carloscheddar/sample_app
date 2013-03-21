534f74de76c769493819ac58641dad241e96165e
Carlos Feliciano <c.feliciano2009@gmail.com>
2013-03-21 00:27:35 -0400

    Added microposts model and specified the relation with the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
+  has_many :microposts
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
